378c5ad9f703fd92fafeee7f44af5ad464afc2a5
Dipesh patidar <94466488+dipeshpatidarjeeran@users.noreply.github.com>
2022-04-26 18:55:38 +0530

    Update intersection_of_two_sets.py

--- intersection_of_two_sets.py
+++ intersection_of_two_sets.py
@@ -4,3 +4,7 @@ print("set1 is :-",s1)
 print("set2 is:-",s2)
 print("intersection of two sets:-",s1&s2)
 print("intersection of two sets:-",s1.intersection(s2))
+s3={8,5,9,2,1}
+s4={4,5,6,1,9}
+s3.intersection_update(s4)
+print(s3)
